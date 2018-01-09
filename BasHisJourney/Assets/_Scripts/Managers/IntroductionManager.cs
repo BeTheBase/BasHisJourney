112e2229224fc59795b0b0e709ec9cb08cc03ade
Bas <bas@warp.industries>
2018-01-09 21:31:29 +0100

    9-1-2018Ienumerators

--- BasHisJourney/Assets/_Scripts/Managers/IntroductionManager.cs
+++ BasHisJourney/Assets/_Scripts/Managers/IntroductionManager.cs
@@ -10,46 +10,64 @@ public class IntroductionManager : MonoBehaviour
     public Button NextButton;
     public Image[] CinematicBars;
     public Image FadeGameIn;
-    public string[] IntroductionText;
+    public string[] IntroductionText, MovementIntroText;
 
-    private string _lastText;
-
+    private string[] _introText;
     private int _value;
+    private bool _performance;
 
     void Awake()
     {
         if (!IntroductionTextObject)
             return;
 
-        IntroductionTextObject.text = IntroductionText[0];
+        _performance = true;
+
+        _introText = IntroductionText;
+
+        IntroductionTextObject.text = _introText[0];
     }
 
     void Start()
     {
         FadeGameIn.CrossFadeAlpha(0f, 3f, true);
-        
+        StartCoroutine(SetInOrActive(FadeGameIn.gameObject, 3, false));
     }
 
     void Update()
     {
-        if (FadeGameIn.color.a < 2f)
-           FadeGameIn.gameObject.SetActive(false);
-
-        if (IntroductionText[IntroductionText.Length -1] == IntroductionText[_value])
+        if (IntroductionText[IntroductionText.Length -1] == IntroductionText[_value] && _performance)
         {
             Debug.Log("Laatste");
             NextButton.gameObject.SetActive(false);
             IntroductionTextObject.CrossFadeAlpha(0,1f,false);
+            StartCoroutine(SetAlphaAfterTime(IntroductionTextObject, 2, 255));
+            _introText = MovementIntroText;
+            IntroductionTextObject.text = _introText[0];
             foreach (var bar in CinematicBars)
             {
-                bar.CrossFadeAlpha(0, 3f, false);
+                bar.CrossFadeAlpha(0, 1f, false);
             }
+
+            _performance = false;
         }
     }
 
     public void NextText()
     {
         _value++;
-        IntroductionTextObject.text = IntroductionText[0 + _value];
+        IntroductionTextObject.text = _introText[0 + _value];
+    }
+
+    IEnumerator SetInOrActive(GameObject obj, int _time, bool _active)
+    {
+        yield return new WaitForSeconds(_time);
+        obj.SetActive(_active);
+    }
+
+    IEnumerator SetAlphaAfterTime(Text obj, int _time, int _amount)
+    {
+        yield return new WaitForSeconds(_time);
+        obj.CrossFadeAlpha(_amount, _time, false);
     }
 }
